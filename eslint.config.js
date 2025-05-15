87253b918326baf733ec10832fce0ca8a650cf34
ImRayy <souradeep_roy@pm.me>
2025-05-15 10:41:08 +0530

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import js from "@eslint/js";
+import pluginQuery from "@tanstack/eslint-plugin-query";
+import pluginRouter from "@tanstack/eslint-plugin-router";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "@tanstack/router": pluginRouter,
+      "@tanstack/query": pluginQuery,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+
+      // Tanstack Router
+      "@tanstack/router/create-route-property-order": "error",
+
+      // Tanstack Query
+      "@tanstack/query/exhaustive-deps": "error",
+      "@tanstack/query/infinite-query-property-order": "warn",
+      "@tanstack/query/no-void-query-fn": "error",
+      "@tanstack/query/no-rest-destructuring": "error",
+    },
+  },
+);
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center">
+      <h1 className="text-4xl font-extrabold">Tsuika 🔖</h1>
+    </div>
+  );
+}
+
+export default App;
