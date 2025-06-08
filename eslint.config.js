95f65089bdb3dd63caa12ce7c7b9e416a78b0d20
ImRayy <souradeep_roy@pm.me>
2025-06-09 00:29:15 +0530

    chore: add eslint-plugin-jsx-a11y for accessibility linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import pluginQuery from "@tanstack/eslint-plugin-query";
 import pluginRouter from "@tanstack/eslint-plugin-router";
+import jsxA11y from "eslint-plugin-jsx-a11y";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
@@ -14,15 +15,23 @@ export default tseslint.config(
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       "@tanstack/router": pluginRouter,
       "@tanstack/query": pluginQuery,
+      "jsx-a11y": jsxA11y,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
+
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
