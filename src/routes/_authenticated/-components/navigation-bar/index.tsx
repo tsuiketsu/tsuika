65c4dbdb0f2aff7fdce07aa20812aae6ff5a9925
ImRayy <souradeep_roy@pm.me>
2025-06-22 11:25:21 +0530

    fix: hide navbar dropdown on non-folder/tag routes to prevent crash when adding items

--- src/routes/_authenticated/-components/navigation-bar/index.tsx
+++ src/routes/_authenticated/-components/navigation-bar/index.tsx
@@ -1,14 +1,23 @@
-import DropdownOptions from "./dropdown-options";
 import { ThemeToggle } from "@/components/theme/theme-toggle";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { useRouterState } from "@tanstack/react-router";
+import { lazy, Suspense } from "react";
+
+const DropdownOptions = lazy(() => import("./dropdown-options"));
 
 export default function NavigationBar() {
+  const {
+    location: { pathname },
+  } = useRouterState();
+
+  const isMenuVisible = pathname.includes("folder") || pathname.includes("tag");
+
   return (
     <div className="bg-background flex h-[53px] items-center gap-2 px-2.5 pt-2">
       <SidebarTrigger />
       <div className="ml-auto inline-flex space-x-2">
         <ThemeToggle />
-        <DropdownOptions />
+        <Suspense>{isMenuVisible && <DropdownOptions />}</Suspense>
       </div>
     </div>
   );
