73dee050de5bac8453fd22639c124ff8a3aea4a1
ImRayy <souradeep_roy@pm.me>
2025-08-07 23:54:36 +0530

    feat(user-cards): show current user as member if no collaborators

--- src/components/forms/folder/collaborate/user-cards.tsx
+++ src/components/forms/folder/collaborate/user-cards.tsx
@@ -1,6 +1,7 @@
 import UserCard from "./user-card";
 import useUserProfile from "@/hooks/user-profile.hook";
 import { userRoles, type Collaborator } from "@/types/folder";
+import { objectPick } from "@/utils";
 import React, { useMemo } from "react";
 
 interface PropsType {
@@ -21,25 +22,40 @@ export default function UserCards({ users, folderId }: PropsType) {
     );
   }, [profile.data?.username, users]);
 
+  const _users = useMemo(() => {
+    return users.length === 0
+      ? [
+          profile.data && {
+            ...objectPick(profile.data, ["name", "image", "username"]),
+            permissionLevel: "owner",
+          },
+        ]
+      : users;
+  }, [profile.data, users]);
+
   return (
     <div className="bg-card space-y-1.5 rounded-md p-2 select-none">
-      {users.map((user, idx) => (
-        <React.Fragment key={`user-${user.username}`}>
-          <UserCard
-            image={user.image ?? ""}
-            name={user.name}
-            username={user.username ?? ""}
-            role={user.permissionLevel}
-            folderId={folderId}
-            isUserAuthorized={
-              user.username !== profile.data?.username
-                ? isUserAuthorized
-                : false
-            }
-          />
-          {users.length - 1 !== idx && <hr />}
-        </React.Fragment>
-      ))}
+      {_users.map((user, idx) => {
+        if (!user) return null;
+
+        return (
+          <React.Fragment key={`user-${user.username}`}>
+            <UserCard
+              image={user.image ?? ""}
+              name={user.name ?? ""}
+              username={user.username ?? ""}
+              role={user.permissionLevel}
+              folderId={folderId}
+              isUserAuthorized={
+                user.username !== profile.data?.username
+                  ? isUserAuthorized
+                  : false
+              }
+            />
+            {users.length > 1 && users.length - 1 !== idx && <hr />}
+          </React.Fragment>
+        );
+      })}
     </div>
   );
 }
