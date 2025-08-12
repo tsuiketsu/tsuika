ffacdcad6222248644555bcaef10bcfe9999d1ba
ImRayy <souradeep_roy@pm.me>
2025-08-12 12:46:39 +0530

    feat(collborator-avatars): show skeletons

--- src/routes/_authenticated/bookmarks/-components/header/collborator-avatars.tsx
+++ src/routes/_authenticated/bookmarks/-components/header/collborator-avatars.tsx
@@ -3,8 +3,22 @@ import { useGetCollaboratorsQuery } from "@/queries/collab-folder.queries";
 import clsx from "clsx";
 import { useMemo } from "react";
 
+const Skeletions = () => {
+  return Array.from({ length: 5 }).map((_, idx) => (
+    <div
+      className="bg-card size-9 rounded-full border"
+      key={`avatar-skeletion-${idx}`}
+      style={{
+        left: idx > 0 ? 24 * idx : 0,
+        zIndex: idx,
+        position: idx > 0 ? "absolute" : "static",
+      }}
+    />
+  ));
+};
+
 export default function CollboratorAvatars({ folderId }: { folderId: string }) {
-  const { data: users } = useGetCollaboratorsQuery(folderId);
+  const { data: users, isFetching } = useGetCollaboratorsQuery(folderId);
 
   const maxItems = 5;
 
@@ -17,20 +31,24 @@ export default function CollboratorAvatars({ folderId }: { folderId: string }) {
   }, [users]);
 
   return (
-    <div className="relative inline-flex">
-      {slicedUsers?.map((user, idx) => (
-        <Avatar
-          key={`collborator-${idx}`}
-          src={user.image || ""}
-          fallback={user.name}
-          className={clsx(
-            "size-9 shadow-sm outline-none",
-            idx > 0 && "absolute left-6"
-          )}
-          style={{ left: idx > 0 ? 24 * idx : 0 }}
-          alt=""
-        />
-      ))}
+    <div className="relative inline-flex select-none">
+      {isFetching ? (
+        <Skeletions />
+      ) : (
+        slicedUsers?.map((user, idx) => (
+          <Avatar
+            key={`collborator-${idx}`}
+            src={user.image || ""}
+            fallback={user.name}
+            className={clsx(
+              "size-9 shadow-sm outline-none",
+              idx > 0 && "absolute left-6"
+            )}
+            style={{ left: idx > 0 ? 24 * idx : 0 }}
+            alt=""
+          />
+        ))
+      )}
       {users && remainingUsers > 0 && (
         <span
           className="bg-card absolute flex size-9 items-center justify-center rounded-full border text-xs shadow-sm"
