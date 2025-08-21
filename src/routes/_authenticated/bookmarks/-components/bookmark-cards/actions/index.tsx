03c739c133b60f94c3bb05dc4fe5fd863710d2f0
ImRayy <souradeep_roy@pm.me>
2025-08-21 12:53:05 +0530

    fix(bookmark): action button alignment & wrong css property

--- src/routes/_authenticated/bookmarks/-components/bookmark-cards/actions/index.tsx
+++ src/routes/_authenticated/bookmarks/-components/bookmark-cards/actions/index.tsx
@@ -59,7 +59,7 @@ export default function BookmarkActions({ bookmark }: { bookmark: Bookmark }) {
   return (
     <div className="group top-2 right-2 z-10 space-x-2 rounded-full">
       <DropdownMenu>
-        <DropdownMenuTrigger className="pointer-cursor hover:scale-95" asChild>
+        <DropdownMenuTrigger className="cursor-pointer hover:scale-95" asChild>
           <Ellipsis size={18} />
         </DropdownMenuTrigger>
         <DropdownMenuContent className="mr-4 w-[224px]">
