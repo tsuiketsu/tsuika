e905ff5b1e02c63d0a4b8abad1a755f48cfc0bd5
ImRayy <souradeep_roy@pm.me>
2025-08-24 12:28:47 +0530

    fix(collaborator-avatars): prevent first avatar skeleton from shrinking

--- src/routes/_authenticated/bookmarks/-components/header/collborator-avatars.tsx
+++ src/routes/_authenticated/bookmarks/-components/header/collborator-avatars.tsx
@@ -7,7 +7,7 @@ import { useMemo } from "react";
 const Skeletions = () => {
   return Array.from({ length: 5 }).map((_, idx) => (
     <div
-      className="bg-card size-9 rounded-full border"
+      className="bg-card size-9 shrink-0 rounded-full border"
       key={`avatar-skeletion-${idx}`}
       style={{
         left: idx > 0 ? 24 * idx : 0,
