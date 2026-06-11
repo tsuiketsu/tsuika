f782244988148b17590cffe0384fa11ee919d4a5
ImRayy <souradeep_roy@pm.me>
2026-06-11 20:17:12 +0530

    chore: update link-preview

--- apps/api/src/utils/link-preview.ts
+++ apps/api/src/utils/link-preview.ts
@@ -3,13 +3,21 @@ import { getLinkPreview } from "link-preview-js";
 import { ERROR_DEFINITIONS } from "@/errors/codes";
 import type { LinkPreviewResponse } from "@/types/link-preview.types";
 
+const userAgents = {
+  mozilla:
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 Chrome/137.0.0.0 Safari/537.36",
+  twitterBot: "Twitterbot",
+};
+
 export const fetchLinkPreview = async (
   websiteUrl: string,
 ): Promise<LinkPreviewResponse> => {
   try {
     const response = await getLinkPreview(websiteUrl, {
       followRedirects: "manual",
-      headers: { "user-agent": "Twitterbot" },
+      headers: {
+        "user-agent": userAgents.mozilla,
+      },
       handleRedirects: () => true,
       resolveDNSHost: async (url: string) => {
         return new Promise((resolve, reject) => {
